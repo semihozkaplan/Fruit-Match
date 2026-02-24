10c8b330ab25b883575804a3b5928b0ca9c9ba81
Semih Özkaplan <semih.ozkaplan@std.hku.edu.tr>
2026-02-25 00:10:45 +0300

    ItemSpotManager is added

---
+++ Assets/GameFolders/Scripts/Item/ItemSpotManager.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+public class ItemSpotManager : MonoBehaviour
+{
+    [Header("Elements")]
+    [SerializeField] private Transform _itemSpot;
+
+    [Header("Settings")]
+    [SerializeField] private Vector3 _itemLocalPosOnSpot;
+    [SerializeField] private Vector3 _itemLocalScaleOnSpot;
+    void Awake()
+    {
+        InputManager.OnItemClicked += HandleItemClicked;
+    }
+
+    private void OnDestroy()
+    {
+       InputManager.OnItemClicked -= HandleItemClicked;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void HandleItemClicked(Item item)
+    {
+        // Turn the item as a child of the item spot
+        item.transform.SetParent(_itemSpot);
+        // Scale the item down, set its loacl position 0,0,0
+        item.transform.localPosition = _itemLocalPosOnSpot;
+        item.transform.localScale = _itemLocalScaleOnSpot;
+        // Disable its shadow
+        item.DisableShadows();
+        // Disable its collider - physics
+        item.DisablePhysics();
+    }   
+}
